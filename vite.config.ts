a1b7704b8724fa78119fc9cbda4a6198cf8dc85f
SusmitParal <nabamitaparal1234@gmail.com>
2026-02-15 20:48:22 +0530

    Add comment for '/my-harmonix/' in vite.config.ts
    Added a comment for the '/my-harmonix/' path.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    /my-harmonix/
     const env = loadEnv(mode, '.', '');
     return {
       server: {
