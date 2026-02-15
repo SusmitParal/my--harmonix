04e9c8f900dbc9346891d1dee2f6db825b44a042
SusmitParal <nabamitaparal1234@gmail.com>
2026-02-15 20:57:18 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    /my-harmonix/
+   base: '/my-harmonix/'
     const env = loadEnv(mode, '.', '');
     return {
       server: {
